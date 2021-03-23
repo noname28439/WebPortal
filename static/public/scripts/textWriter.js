9ceee5dc8204adcae8ed269eae0916a12f115e7c
noname28439 <dohle.leon@web.de>
2021-03-23 18:53:25 +0100

    Basic Design

---
+++ static/public/scripts/textWriter.js
@@ -0,0 +1,29 @@
+
+
+var toRewriteList = document.getElementsByClassName("retype")
+
+for(let i = 0; i<toRewriteList.length; i++){
+    currentElement = toRewriteList[i]
+    //console.log("retype: "+currentElement);
+    rewrite(currentElement);
+}
+
+function rewrite(element){
+    let content = element.innerHTML;
+    
+    element.innerHTML = "";
+    let i = -10;
+    let animationContent = "";
+    let itervalID = setInterval(function() {
+        if(i >= 0){
+            animationContent += content.split("")[i];
+            element.innerHTML = animationContent;
+            if(i>content.length-2)
+                clearInterval(itervalID);
+        }
+        i+=1
+        }, 100)
+}
+
+
+
